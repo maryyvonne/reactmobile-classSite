1c5ca2a6b4739a85df1fc64d44cad0c4d3f55e27
Mary Varnado <59319147+maryyvonne@users.noreply.github.com>
2022-08-03 11:34:28 -0500

    Alert

--- features/comments/commentsSlice.js
+++ features/comments/commentsSlice.js
@@ -1,18 +1,44 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { baseUrl } from "../../shared/baseUrl";
 
-export const fetchComments = createAsyncThunk(
-  "comments/fetchComments",
-  async () => {
-    const response = await fetch(baseUrl + "comments");
-    return response.json();
+const fetchComments = createAsyncThunk("comments/fetchComments", async () => {
+  const response = await fetch(baseUrl + "comments");
+  return response.json();
+});
+
+const postComment = createAsyncThunk(
+  "comments/postComment",
+  async (payload, { dispatch, getState }) => {
+    setTimeout(() => {
+      const { comments } = getState();
+      payload.date = (new Date()).toISOString();      ;
+      payload.id = comments.commentsArray.length;
+
+      dispatch(addComment(payload));
+    }, 2000);
   }
 );
+
+// const initialState = {
+//   commentsArray: [],
+//   isLoading: true,
+//   errMsg: ""
+// };
 
 const commentsSlice = createSlice({
   name: "comments",
   initialState: { isLoading: true, errMess: null, commentsArray: [] },
-  reducers: {},
+  reducers: {
+    addComment: (state, action) => {
+      console.log("addComment action.payload:", action.payload);
+      console.log("addComment state.commentsArray:", state.commentsArray);
+      const newComment = {
+        id: state.commentsArray.length + 1,
+        ...action.payload,
+      };
+      state.commentsArray.push(action.payload);
+    },
+  },
   extraReducers: {
     [fetchComments.pending]: (state) => {
       state.isLoading = true;
@@ -30,3 +56,5 @@ const commentsSlice = createSlice({
 });
 
 export const commentsReducer = commentsSlice.reducer;
+export const { addComment } = commentsSlice.actions;
+export { postComment, fetchComments };
